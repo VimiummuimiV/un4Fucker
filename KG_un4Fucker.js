5e2dfda5d0e0b8d63631c654793f21999326e7eb
Puncher <enragedracer@gmail.com>
2020-11-14 03:27:09 +0200

    Next Text Info Fix

--- KG_un4Fucker.js
+++ KG_un4Fucker.js
@@ -4,13 +4,34 @@
 // @version      0.5
 // @description  Let this guy suffer!
 // @author       Puncher
-// @match        http*://klavogonki.ru/g*
+// @match        http*://klavogonki.ru/gamelist/
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-// Global dynamical variables
 var sentences;
+var sentence;
+// Get sentences database from github txt file
+(async() => {
+    var url = 'https://raw.githubusercontent.com/VimiummuimiV/TXT_FILES/main/KG_Sentences.txt';
+    var response = await fetch(url);
+    var data = await response.text();
+    sentences = data.split("\n");
+})();
+
+setTimeout(() => {
+
+// Random sentence with no repeat
+var oneRepeatSentence = shuffle(sentences);
+// Randomize sentences array index from database with repeat no more than 1
+function* shuffle(array) {
+    var i = sentences.length;
+    while (i--) {
+        yield sentences.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+    }
+}
+// Milliseconds
 var milliseconds;
+// Users
 var user;
 var pereborich;
 var snowman;
@@ -22,10 +43,10 @@ var field = document.querySelector('.text');
 var inject = document.querySelector('.send');
 // Randomize seconds
 function generateRandomInteger(min, max) {
-    return Math.floor(min + Math.random()*(max + 1 - min))
+    return Math.floor(min + Math.random() * (max + 1 - min));
 }
 // Update every second users availability in chat list
-setInterval(function(){
+setInterval(function () {
     // Pay attention
     user = document.querySelector('.userlist-content .user111001');
     // Be aware
@@ -35,72 +56,67 @@ setInterval(function(){
     // For condition
     danger = pereborich || snowman || danieli;
 }, 1000);
-// Get sentences database from github txt file
-(async() => {
-    var url = 'https://raw.githubusercontent.com/VimiummuimiV/TXT_FILES/main/KG_Sentences.txt';
-    var response = await fetch(url);
-    var data = await response.text();
-    sentences = data.split("\n");
-})();
-// Randomize sentences array index from database with repeat no more than 1
-function* shuffle(array) {
-    var i = sentences.length;
-    while (i--) {
-        yield sentences.splice(Math.floor(Math.random() * (i+1)), 1)[0];
-    }
-}
-// Random sentence with no repeat
-var oneRepeatSentence = shuffle(sentences);
+
+// Creating Indicator
+var chatPanel = document.querySelector('.dummy');
+var indicator = chatPanel.appendChild(document.createElement('p'));
+var nextSentence = chatPanel.appendChild(document.createElement('p'));
+
+indicator.style.cssText =
+    'display: flex;' +
+    'height: 16px;' +
+    'width: 32px;' +
+    'background: #213434;' +
+    'justify-content: center;' +
+    'position: absolute;' +
+    'top: 0;' +
+    'right: 105px;' +
+    'align-items: center;' +
+    'font: 12px Consolas;' +
+    'border: 1px solid burlywood;' +
+    'color: burlywood;' +
+    'z-index: 1;';
+
+nextSentence.style.cssText =
+    'display: block;' +
+    'position: absolute;' +
+    'font-size: 10px;' +
+    'top: 2px;' +
+    'color: gray;' +
+    'left: 100px;';
 
 // Inject sentence in chat
 function initialize() {
-    field.value = `un4given + ${oneRepeatSentence.next().value.toLowerCase()}`;
+    field.value = `un4given, ${sentence}`;
     inject.click();
 };
 
 // Repeat with interval initialize function
 (function loop() {
+    sentence = oneRepeatSentence.next().value.toLowerCase();
+    nextSentence.innerText = sentence;
     milliseconds = generateRandomInteger(400000, 600000);
-    setTimeout(function() {
-            // Don't run if moderator in chat or badass is absent
-            if (danger || user == null) {
-               void(0); // Do nothing 
-            } else {
-                initialize();
-            }
+    setTimeout(function () {
+        // Don't run if moderator in chat or badass is absent
+        if (danger || user == null) {
+            void (0); // Do nothing 
+        } else {
+            initialize();
+        }
         loop();
     }, milliseconds);
-}());
-
-// Creating Indicator and Stylize it
-var chatPanel = document.querySelector('.dummy');
-var indicator = chatPanel.appendChild(document.createElement('p'));
-
-indicator.style.cssText =
-'display: flex;' +
-'height: 16px;' +
-'width: 32px;' +
-'background: #213434;' +
-'justify-content: center;' +
-'position: absolute;' +
-'top: 0;' +
-'right: 105px;' +
-'align-items: center;' +
-'font: 12px Consolas;' +
-'border: 1px solid burlywood;' +
-'color: burlywood;' +
-'z-index: 1;';
+} ());
 
 // Indicator functions
 (function () {
-var nativeSetTimeout = window.setTimeout;
+    var nativeSetTimeout = window.setTimeout;
 
-window.bindTimeout = function (listener, interval) {
-    function setTimeout(code, delay) {
-        var elapsed = 0,
-            h;
+    window.bindTimeout = function (listener, interval) {
+        function setTimeout(code, delay) {
+            var elapsed = 0,
+                h;
 
-        h = window.setInterval(function () {
+            h = window.setInterval(function () {
                 elapsed += interval;
                 if (elapsed < delay) {
                     listener(delay - elapsed);
@@ -108,13 +124,13 @@ window.bindTimeout = function (listener, interval) {
                     window.clearInterval(h);
                 }
             }, interval);
-        return nativeSetTimeout(code, delay);
-    }
+            return nativeSetTimeout(code, delay);
+        }
 
-    window.setTimeout = setTimeout;
-    setTimeout._native = nativeSetTimeout;
-};
-}());
+        window.setTimeout = setTimeout;
+        setTimeout._native = nativeSetTimeout;
+    };
+} ());
 // Show how much seconds left
 window.bindTimeout(function (ms) {
     if (danger) {
@@ -129,4 +145,6 @@ window.bindTimeout(function (ms) {
     }
 }, 1000);
 // Show what sentence will be sended in console
-window.setTimeout(function(){}, milliseconds);
+window.setTimeout(() => { }, milliseconds);
+
+}, 2000);
